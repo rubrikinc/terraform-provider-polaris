ddfd506e8de85371a3a5d02b050994a11c878c1a
Johan Andersson <johan.andersson@rubrik.com>
2023-05-02 21:08:11 +0200

    Remove example Azure secret

--- examples/resources/polaris_azure_service_principal/resource.tf
+++ examples/resources/polaris_azure_service_principal/resource.tf
@@ -15,7 +15,7 @@ resource "polaris_azure_service_principal" "default" {
 resource "polaris_azure_service_principal" "default" {
   app_id        = "25c2b42a-c76b-11eb-9767-6ff6b5b7e72b"
   app_name      = "My App"
-  app_secret    = "H12w-c.g3mjk47iz1qyFltv~zqD9m4cpK_"
+  app_secret    = "<my-apps-secret>"
   tenant_domain = "mydomain.onmicrosoft.com"
   tenant_id     = "2bfdaef8-c76b-11eb-8d3d-4706c14a88f0"
 }
