95fc5f63adefd36c40fb4843fd73ff68ab410191
Johan Andersson <johan.andersson@rubrik.com>
2023-11-14 19:40:24 +0100

    Fix how the provider is looked up in AWS CNP resources

--- internal/provider/data_source_features.go
+++ internal/provider/data_source_features.go
@@ -29,7 +29,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris"
 	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/graphql/core"
 )
 
@@ -56,7 +55,10 @@ func dataSourceFeatures() *schema.Resource {
 func featuresRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	log.Print("[TRACE] featuresRead")
 
-	client := m.(*polaris.Client)
+	client, err := m.(*client).polaris()
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
 	// Request features.
 	features, err := core.Wrap(client.GQL).AllEnabledFeaturesForAccount(ctx)
--- internal/provider/resource_aws_cnp_account.go
+++ internal/provider/resource_aws_cnp_account.go
@@ -29,7 +29,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris"
 	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/aws"
 	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/graphql"
 	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/graphql/core"
@@ -105,7 +104,10 @@ func resourceAwsCnpAccount() *schema.Resource {
 func awsCreateCnpAccount(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	log.Print("[TRACE] awsCreateCnpAccount")
 
-	client := m.(*polaris.Client)
+	client, err := m.(*client).polaris()
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
 	// Get attributes.
 	cloud := d.Get("cloud").(string)
@@ -136,7 +138,10 @@ func awsCreateCnpAccount(ctx context.Context, d *schema.ResourceData, m interfac
 func awsReadCnpAccount(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	log.Print("[TRACE] awsReadCnpAccount")
 
-	client := m.(*polaris.Client)
+	client, err := m.(*client).polaris()
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
 	// Get attributes.
 	id, err := uuid.Parse(d.Id())
@@ -187,7 +192,10 @@ func awsReadCnpAccount(ctx context.Context, d *schema.ResourceData, m interface{
 func awsUpdateCnpAccount(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	log.Print("[TRACE] awsUpdateCnpAccount")
 
-	client := m.(*polaris.Client)
+	client, err := m.(*client).polaris()
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
 	// Get attributes.
 	id, err := uuid.Parse(d.Id())
@@ -270,7 +278,10 @@ func awsUpdateCnpAccount(ctx context.Context, d *schema.ResourceData, m interfac
 func awsDeleteCnpAccount(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	log.Print("[TRACE] awsDeleteCnpAccount")
 
-	client := m.(*polaris.Client)
+	client, err := m.(*client).polaris()
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
 	// Get attributes.
 	id, err := uuid.Parse(d.Id())
