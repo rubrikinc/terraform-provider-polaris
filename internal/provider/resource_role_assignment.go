f0da801fb033171da1f404e770191387e26509c2
Johan Andersson <johan.andersson@rubrik.com>
2024-01-30 18:53:07 +0100

    Drop resources removed from RSC (#114)

--- internal/provider/resource_role_assignment.go
+++ internal/provider/resource_role_assignment.go
@@ -23,6 +23,7 @@ package provider
 import (
 	"context"
 	"crypto/sha256"
+	"errors"
 	"fmt"
 	"log"
 
@@ -31,6 +32,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/access"
+	"github.com/rubrikinc/rubrik-polaris-sdk-for-go/pkg/polaris/graphql"
 )
 
 // resourceRoleAssignment defines the schema for the role assignment resource.
@@ -101,6 +103,10 @@ func readRoleAssignment(ctx context.Context, d *schema.ResourceData, m any) diag
 	userEmail := d.Get("user_email").(string)
 
 	user, err := access.Wrap(client).User(ctx, userEmail)
+	if errors.Is(err, graphql.ErrNotFound) {
+		d.SetId("")
+		return nil
+	}
 	if err != nil {
 		return diag.FromErr(err)
 	}
