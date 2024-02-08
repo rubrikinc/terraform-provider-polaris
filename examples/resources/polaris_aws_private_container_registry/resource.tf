f34c63868e7e5a95b3556f98185fddbfb910b642
Johan Andersson <johan.andersson@rubrik.com>
2024-02-08 11:05:56 +0100

    Update PCR example

--- examples/resources/polaris_aws_private_container_registry/resource.tf
+++ examples/resources/polaris_aws_private_container_registry/resource.tf
@@ -1,4 +1,5 @@
 resource "polaris_aws_private_container_registry" "default" {
   account_id = polaris_aws_account.default.id
-  url        = "123456789012.dkr.ecr.us-east-2.amazonaws.com"
+  native_id  = "123456789012"
+  url        = "234567890121.dkr.ecr.us-east-2.amazonaws.com"
 }
