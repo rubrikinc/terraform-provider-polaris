3da56cf45c1a0801f8d8c51aa97e0982623af75c
Johan Andersson <johan.andersson@rubrik.com>
2024-09-26 20:31:25 +0200

    Add support for managing SLA domains

---
+++ examples/resources/polaris_sla_domain/resource.tf
@@ -0,0 +1,6 @@
+resource "polaris_sla_domain" "daily" {
+  name = "daily"
+}
+resource "polaris_sla_domain" "weekly" {
+  name = "weekly"
+}
--- internal/provider/data_source_sla_domain.go
+++ internal/provider/data_source_sla_domain.go
@@ -37,6 +37,13 @@ The ´polaris_sla_domain´ data source is used to access information about RSC S
 domains. A SLA domain is looked up using either the ID or the name.
 `
 
+const (
+	keyArchivalSpec  = "archival_spec"
+	keyThreshold     = "threshold"
+	keyThresholdUnit = "threshold_unit"
+	keyFrequencies   = "frequencies"
+)
+
 func dataSourceSLADomain() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: slaDomainRead,
@@ -69,6 +76,210 @@ func dataSourceSLADomain() *schema.Resource {
 				},
 				Computed:    true,
 				Description: "Object types which can be protected by the SLA domain.",
+			},
+			keyArchivalSpec: {
+				Type:        schema.TypeSet,
+				Elem:        archivalSpecResource(),
+				Computed:    true,
+				Description: "",
+			},
+			keyDailySchedule: {
+				Type: schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						keyFrequency: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Frequency of snapshots (days).",
+						},
+						keyRetention: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Retention of snapshots.",
+						},
+						keyRetentionUnit: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Retention unit.",
+						},
+					},
+				},
+				Computed:    true,
+				Description: "Daily schedule of the SLA Domain.",
+			},
+			keyHourlySchedule: {
+				Type: schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						keyFrequency: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Frequency of snapshots (hours).",
+						},
+						keyRetention: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Retention.",
+						},
+						keyRetentionUnit: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Retention unit.",
+						},
+					},
+				},
+				Computed:    true,
+				Description: "Hourly schedule.",
+			},
+			keyMinuteSchedule: {
+				Type: schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						keyFrequency: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Frequency (minutes).",
+						},
+						keyRetention: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Retention.",
+						},
+						keyRetentionUnit: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Retention unit.",
+						},
+					},
+				},
+				Computed:    true,
+				Description: "Minute schedule.",
+			},
+			keyMonthlySchedule: {
+				Type: schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						keyFrequency: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Frequency (months).",
+						},
+						keyRetention: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Retention.",
+						},
+						keyRetentionUnit: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Retention unit.",
+						},
+						keyDayOfMonth: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Day of month.",
+						},
+					},
+				},
+				Computed:    true,
+				Description: "Monthly schedule.",
+			},
+			keyQuarterlySchedule: {
+				Type: schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						keyFrequency: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Frequency (quarters).",
+						},
+						keyRetention: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Retention.",
+						},
+						keyRetentionUnit: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Retention unit.",
+						},
+						keyDayOfQuarter: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Day of quarter.",
+						},
+						keyQuarterStartMonth: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Quarter start month.",
+						},
+					},
+				},
+				Computed:    true,
+				Description: "Quarterly schedule.",
+			},
+			keyWeeklySchedule: {
+				Type: schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						keyFrequency: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Frequency (weeks).",
+						},
+						keyRetention: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Retention.",
+						},
+						keyRetentionUnit: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Retention unit.",
+						},
+						keyDayOfWeek: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Day of week.",
+						},
+					},
+				},
+				Computed:    true,
+				Description: "Weekly schedule.",
+			},
+			keyYearlySchedule: {
+				Type: schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						keyFrequency: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Frequency (years).",
+						},
+						keyRetention: {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Retention.",
+						},
+						keyRetentionUnit: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Retention unit.",
+						},
+						keyDayOfYear: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Day of year.",
+						},
+						keyYearStartMonth: {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Year start month.",
+						},
+					},
+				},
+				Computed:    true,
+				Description: "Yearly schedule.",
 			},
 		},
 	}
@@ -114,6 +325,95 @@ func slaDomainRead(ctx context.Context, d *schema.ResourceData, m any) diag.Diag
 		return diag.FromErr(err)
 	}
 
+	if archivalSpecs := toArchivalSpecs(slaDomain); archivalSpecs != nil {
+		if err := d.Set(keyArchivalSpec, archivalSpecs); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	if daily := toDailySchedule(slaDomain); daily != nil {
+		if err := d.Set(keyDailySchedule, daily); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	if hourly := toHourlySchedule(slaDomain); hourly != nil {
+		if err := d.Set(keyHourlySchedule, hourly); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	if minutely := toMinuteSchedule(slaDomain); minutely != nil {
+		if err := d.Set(keyMinuteSchedule, minutely); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	if monthly := toMonthlySchedule(slaDomain); monthly != nil {
+		if err := d.Set(keyMonthlySchedule, monthly); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	if quarterly := toQuarterlySchedule(slaDomain); quarterly != nil {
+		if err := d.Set(keyQuarterlySchedule, quarterly); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	if weekly := toWeeklySchedule(slaDomain); weekly != nil {
+		if err := d.Set(keyWeeklySchedule, weekly); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	if yearly := toYearlySchedule(slaDomain); yearly != nil {
+		if err := d.Set(keyYearlySchedule, yearly); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
 	d.SetId(slaDomain.ID.String())
 	return nil
+}
+
+func archivalSpecResource() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			keyFrequencies: {
+				Type: schema.TypeSet,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Computed:    true,
+				Description: "",
+			},
+			keyThreshold: {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "",
+			},
+			keyThresholdUnit: {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "",
+			},
+		},
+	}
+}
+
+func toArchivalSpecs(slaDomain gqlsla.Domain) *schema.Set {
+	if len(slaDomain.ArchivalSpecs) == 0 {
+		return nil
+	}
+
+	archivalSpecs := &schema.Set{F: schema.HashResource(archivalSpecResource())}
+	for _, archivalSpec := range slaDomain.ArchivalSpecs {
+		frequencies := &schema.Set{F: schema.HashString}
+		for _, freq := range archivalSpec.Frequencies {
+			frequencies.Add(string(freq))
+		}
+
+		archivalSpecs.Add(map[string]interface{}{
+			keyFrequencies:   frequencies,
+			keyThreshold:     archivalSpec.Threshold,
+			keyThresholdUnit: string(archivalSpec.ThresholdUnit),
+		})
+	}
+
+	return archivalSpecs
 }
