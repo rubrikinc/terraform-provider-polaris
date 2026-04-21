002de56e1f488e2fbeb048543d75de381411826e
Johan Andersson <johan.andersson@rubrik.com>
2026-04-21 14:43:09 +0200

    Add identity import examples for search-enabled resources
    Add import-by-identity.tf examples for polaris_custom_role,
    polaris_user, and polaris_sso_group. Also add the missing
    import-by-string-id.tf for polaris_sso_group.

---
+++ examples/resources/polaris_sso_group/import-by-identity.tf
@@ -0,0 +1,6 @@
+import {
+  to = polaris_sso_group.auditors
+  identity = {
+    id = "a3f4b5c6-d7e8-9012-3456-789abcdef012"
+  }
+}
---
+++ examples/resources/polaris_sso_group/import-by-string-id.tf
@@ -0,0 +1,4 @@
+import {
+  to = polaris_sso_group.auditors
+  id = "Auditors:1a5629cb-2681-4ea4-b36c-ea8b2f3990cd"
+}
